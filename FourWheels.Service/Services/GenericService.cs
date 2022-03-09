d38551550b76c4793c0c082943c37b0436c00e7c
Casper <casper.koch.dev@gmail.com>
2022-03-09 10:51:08 +0100

    Added GenericService.cs

---
+++ FourWheels.Service/Services/GenericService.cs
@@ -0,0 +1,36 @@
+﻿using FourWheels.Repository.Interfaces;
+using FourWheels.Service.Interfaces;
+
+namespace FourWheels.Service.Services
+{
+    /// <summary>
+    /// Generic service
+    /// </summary>
+    /// <typeparam name="T">The class type</typeparam>
+    public abstract class GenericService<T, I> : IGenericService<T> where T : class where I : IGenericRepository<T>
+    {
+        private readonly I _genericRepository;
+        private readonly string _entityName = typeof(T).ToString().Replace("Repository.", "");
+
+        protected GenericService(I genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await _genericRepository.CreateAsync(entity);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            await _genericRepository.UpdateAsync(entity);
+        }
+
+        public async Task<List<T>> GetAllAsync()=>
+            await _genericRepository.GetAllAsync();
+
+        public Task<T> GetByIdAsync(object id)=>
+            _genericRepository.GetByIdAsync(id);
+    }
+}
