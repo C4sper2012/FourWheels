4fcff1cd3fb5b3411b0bb45240442d3a95b217e5
tobinieil04 <tobiniel04@gmail.com>
2022-03-09 10:26:21 +0100

    Added CreateBil Razor Page

---
+++ FourWheels.web/Pages/CreateBil.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FourWheels.web.Pages
+{
+    public class CreateBilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
