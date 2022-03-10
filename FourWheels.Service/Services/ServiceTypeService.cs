c725ec38537db63b37384750ac634d7e359a453a
Jan Andreasen <jan@tved.it>
2022-03-10 09:23:34 +0100

    Added servicetype-service scaffolding

--- FourWheels.Service/Services/ServiceTypeService.cs
+++ FourWheels.Service/Services/ServiceTypeService.cs
@@ -1,9 +1,44 @@
-﻿using FourWheels.Repository.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FourWheels.Repository.Domain;
+using FourWheels.Repository.Entities;
 using FourWheels.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-namespace FourWheels.Service.Services;
-
-public class ServiceTypeService : GenericService<Servicetype>, IServiceTypeService
+namespace FourWheels.Service.Services
 {
-    
+    public class ServiceTypeService : GenericService<Servicetype>, IServiceTypeService
+    {
+        private readonly FourWheelsContext _db;
+
+        public ServiceTypeService(FourWheelsContext db)
+        {
+            _db = db;
+        }
+
+        public Task CreateAsync(Servicetype entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Servicetype entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Servicetype>> GetAllAsync()
+        {
+            return _db.Biler.ToListAsync();
+        }
+
+        public Task<Servicetype> GetByIdAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GenericService<T>
+    {
+    }
 }
