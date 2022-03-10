eba4fbb7f1b997052f7d37d6e491dd8e568e7049
Peter Hymøller <pete102c@elevcampus.dk>
2022-03-10 13:11:57 +0100

    maby working

--- FourWheels.UI/Pages/ArbejdsOrdreListe.cshtml.cs
+++ FourWheels.UI/Pages/ArbejdsOrdreListe.cshtml.cs
@@ -1,3 +1,4 @@
+using FourWheels.Repository.Entities;
 using FourWheels.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,19 +7,19 @@ namespace FourWheels.UI.Pages
 {
     public class ArbejdsOrdreListeModel : PageModel
     {
-        private readonly IBilService _bilService;
-        private readonly IKundeService _kundeService;
-        private readonly IServiceTypeService _serviceTypeService;
-        public ArbejdsOrdreListeModel(IBilService bilService, IKundeService kundeService, IServiceTypeService serviceTypeService)
+        private readonly IArbejdsOrdrerService _arbejdsOrdrerService;
+        public ArbejdsOrdreListeModel(IArbejdsOrdrerService arbejdsOrdrerService)
         {
-            _bilService = bilService;
-            _kundeService = kundeService;
-            _serviceTypeService = serviceTypeService;
+            _arbejdsOrdrerService = arbejdsOrdrerService;
         }
 
+        [BindProperty(SupportsGet = true)]
+        public List<Arbejdsordrer> Arbejdsordrers { get; set; }
 
-        public void OnGet()
+
+        public async Task OnGet()
         {
+            Arbejdsordrers = await _arbejdsOrdrerService.GetAllAsync();
         }
     }
 }
