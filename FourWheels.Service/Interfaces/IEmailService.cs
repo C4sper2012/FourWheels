89f2119f6c453f19de4cbf31621067474ddc39ed
tobinieil04 <tobiniel04@gmail.com>
2022-03-11 09:02:08 +0100

    Started EmailService

---
+++ FourWheels.Service/Interfaces/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using FourWheels.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourWheels.Service.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmail(Arbejdsordrer arbejdsordrer);
+    }
+}
---
+++ FourWheels.Service/Services/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using FourWheels.Repository.Entities;
+using FourWheels.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourWheels.Service.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IKundeService _kundeService;
+
+        public EmailService(IKundeService kundeService)
+        {
+            _kundeService = kundeService;
+        }
+
+        public async Task SendEmail(Arbejdsordrer arbejdsordrer)
+        {
+
+            Kunde kunde = await _kundeService.GetByIdAsync(arbejdsordrer.Bil.FKEjer);
+
+            using (var smtp = new SmtpClient("127.0.0.1", 25))
+            {
+                var message = new MailMessage
+                {
+                    Subject = "Din bil er færdig!",
+                    Body = $"Hej {kunde.Fuldenavn}" +
+                           "Din bil er nu færdig og kan afhentes.",
+
+
+                    From = new MailAddress("fourWheels@mail.dk")
+                };
+
+                message.To.Add($"{arbejdsordrer.Bil.Kunde.Email}");
+                await smtp.SendMailAsync(message);
+            }
+        }
+
+    }
+}
