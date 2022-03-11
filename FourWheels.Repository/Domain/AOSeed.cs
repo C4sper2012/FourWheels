de6391b68e5d8fb3942f55f5e770b8066bdf2355
Casper <casper.koch.dev@gmail.com>
2022-03-11 12:12:17 +0100

    Added details and seeddata

--- FourWheels.Repository/Domain/AOSeed.cs
+++ FourWheels.Repository/Domain/AOSeed.cs
@@ -11,17 +11,32 @@ namespace FourWheels.Repository.Domain
             builder.Entity<Arbejdsordrer>().HasData(
                 new Arbejdsordrer
                 {
-                    PKArbejdsordreID = 1, FKBilID = 1, Oprettet = DateTime.Now,
+                    PKArbejdsordreID = 1, FKBilID = 1, FKMekanikerID = 3, Oprettet = DateTime.Now,
                     FKServiceID = 3, 
                 },
                 new Arbejdsordrer
                 {
-                    PKArbejdsordreID = 2, FKBilID = 3, FKMekanikerID = 1, Oprettet = DateTime.Now,
+                    PKArbejdsordreID = 2, FKBilID = 1, FKMekanikerID = 2, Oprettet = DateTime.Now,
+                    FKServiceID = 1, 
+                },
+                new Arbejdsordrer
+                {
+                    PKArbejdsordreID = 3, FKBilID = 1, FKMekanikerID = 1, Oprettet = DateTime.Now,
+                    FKServiceID = 2, 
+                },
+                new Arbejdsordrer
+                {
+                    PKArbejdsordreID = 4, FKBilID = 1, FKMekanikerID = 1, Oprettet = DateTime.Now,
+                    FKServiceID = 2, 
+                },
+                new Arbejdsordrer
+                {
+                    PKArbejdsordreID = 5, FKBilID = 3, FKMekanikerID = 1, Oprettet = DateTime.Now,
                     FKServiceID = 5
                 },
                 new Arbejdsordrer
                 {
-                    PKArbejdsordreID = 3, FKBilID = 4, FKMekanikerID = 2, Oprettet = DateTime.Now,
+                    PKArbejdsordreID = 6, FKBilID = 4, FKMekanikerID = 2, Oprettet = DateTime.Now,
                     FKServiceID = 2, TidBrugt = DateTime.Now.AddHours(-3)
                 }
             );  
